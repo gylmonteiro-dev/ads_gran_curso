ef51ad4d27aba1f4545a0a57771b6f5aafc5907e
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2025-01-05 08:48:22 -0300

    criado a serie de fibonacci com recurssividade na prática

---
+++ PRÁTICAS_DE_PROGRAMAÇÃO/UA-3_COLEÇÕES_E_FUNÇÕES/imersao_javascript_colecoes_e_funcoes/pratica/fibonnaci_com_recursao/script.js
@@ -0,0 +1,13 @@
+function fibonacci(n){
+    if(n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fibonacci(n-1) + fibonacci(n - 2);
+}
+
+
+for (let n=0; n<10; n++){
+    console.log(fibonacci(n));
+}
