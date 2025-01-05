5fb54ce2d6b71cd20a245291252f191f79c189ec
Gyl Monteiro <gyl.tecno.especialista@gmail.com>
2025-01-04 21:01:16 -0300

    pasta_praticas

---
+++ PRÁTICAS_DE_PROGRAMAÇÃO/UA-3_COLEÇÕES_E_FUNÇÕES/recursividade/pratica/fatorail_co_recursividade/script.js
@@ -0,0 +1,11 @@
+function fatorial() {
+    const valorUsuario = Number(document.getElementById('numero').value)
+    const containerResultado = document.getAnimations('resultado_fatorial')
+
+    let totalFatorial = valorUsuario
+    let tempValue = valorUsuario - 1
+    
+    totalFatorial =
+
+    
+}
